bb22e12e115f6e028ab10e374e1680dc2244c2bc
Seungmin Ha <48918003+Seung-minnn@users.noreply.github.com>
2020-03-03 02:05:21 +0900

    백준통과 정올95점...우선 포기합니다ㅠㅠㅠ

--- JungOl/C1603 (경비행기)/hsm.cpp
+++ JungOl/C1603 (경비행기)/hsm.cpp
@@ -1,100 +1,84 @@
 #include<stdio.h>
 #include<iostream>
 #include<queue>
-#include<vector>
 #include<algorithm>
 #include<cmath>
 
 using namespace std;
 
 int n, k;
-int sx, sy, ex, ey; //시작지점, 끝지점
-int ans = 987654321;
-
-queue<pair<int, int>> q;
-
-vector<pair<int, int>> v; //n개의 경유지들 좌표
-vector<int> d; //data 0~n-1 저장되어서 v의 인덱스 표시하기 위해 만든 변수
+int visited[1010];
+pair<int, int> a_list[1010];
+int ans;
 
 /*
-부분집합 구하는 함수로 k개이하일때 경로를 구해 작은 연료통 값을 ans에 대입하는 방식으로
-생각했는데ㅠㅠㅠ..시간 초과가 뜨네요 8ㅡ8 우선 더 고민해볼게요...
+인터넷 참고해서 이분탐색 아이디어 얻어서 풀었는디...
+백준에서는 통과하는데
+정올에서는 95점 나오네요...ㅠㅠㅠㅠ
+더 고민해보고 고칠게요
 */
-
-void make_answer(queue<int> q) {
-	int oil = -1;
-	while (!q.empty()) {
-		//printf("q: %d\n", q.front());
-		int x = v[q.front()].first;
-		int y = v[q.front()].second;
-		q.pop();
-		//printf("x,y: %d %d\n", x, y);
-
-		int tmp = floor(sqrt((sx - x)*(sx - x) + (sy - y)*(sy - y)));
-		if (tmp % 10 != 0) {
-			tmp = tmp / 10 + 1;
-		}
-		else {
-			tmp /= 10;
-		}
-
-		//printf("tmp: %d\n", tmp);
-		if (oil < tmp) {
-			oil = tmp;
-		}
-		
-		sx = x;
-		sy = y;
-	}
-
-	int tmp = floor(sqrt((sx - ex)*(sx - ex) + (sy - ey)*(sy - ey)));
-	if (tmp % 10 != 0) {
-		tmp = tmp / 10 + 1;
-	}
-	else {
-		tmp /= 10;
-	}
-	//printf("tmp: %d\n", tmp);
-	if (oil < tmp) {
-		oil = tmp;
-	}
-
-	//printf("\n%d\n---------------\n", oil);
-	if (ans > oil) {
-		ans = oil;
-	}
-}
-
-void powerset(int n) {
-	int max = 1 << n;
-	for (int i = 0; i < max; i++) {
-		queue<int> tmp;
-		sx = 0, sy = 0, ex = 10000, ey = 10000;
-		for (int j = 0; j < n; j++) {
-			if (i&(1 << j)) {
-				tmp.push(d[j]);
-			}
-		}
-		if (tmp.size()<=k) {
-			make_answer(tmp);
-		}
-	}
-}
 
 int main() {
 	scanf("%d %d", &n, &k);
 	int temp = 0;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		int t1, t2;
 		scanf("%d %d", &t1, &t2);
-		v.push_back({ t1,t2 });
-		d.push_back(temp++);
-		f.push_back(0);
+		a_list[i]={ t1,t2 };
 	}
+	a_list[n + 1] = { 10000,10000 };
+
+	int l = 1, r = 14144;
 	
-	powerset(d.size());
-	
+	while (l <= r) {
+		queue<int> q1;
+		queue<int> q2;
+		int flag = 0, mid = (l + r) / 2;
+		for (int i = 1; i <= n + 1; i++) {
+			visited[i] = 0;
+		}
+		q1.push(0); q2.push(0);
+		visited[0] = 1;
+
+		while (!q1.empty()) {
+			int cur = q1.front();
+			int cnt = q2.front();
+			q1.pop(); q2.pop();
+
+			if (cur == n + 1) {
+				flag = 1;
+				break;
+			}
+
+			if (cnt == k + 1) {
+				continue;
+			}
+
+			for (int i = 1; i <= n + 1; i++) {
+				if (!visited[i] && mid*mid > (a_list[cur].first - a_list[i].first)*(a_list[cur].first - a_list[i].first) + (a_list[cur].second - a_list[i].second)*(a_list[cur].second - a_list[i].second)) {
+					visited[i] = 1;
+					q1.push(i);
+					q2.push(cnt + 1);
+				}
+			}
+		}
+		if (flag) {
+			r = mid - 1;
+		}
+		else {
+			l = mid + 1;
+		}
+	}
+
+	if (l % 10 != 0) {
+		ans = l / 10 + 1;
+	}
+	else {
+		ans = l / 10;
+	}
+
 	printf("%d\n", ans);
 	//system("pause");
 	return 0;
 }
+
